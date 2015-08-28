c0ed67a9d0375dc68b327614a7cb589a88e7264c
Jary <jaryroxas@gmail.com>
2015-08-29 01:35:45 +0800

    continue modifying homepage, update content-home to get the proper stylesheet

--- wp-content/themes/kqed/template-parts/content-home.php
+++ wp-content/themes/kqed/template-parts/content-home.php
@@ -10,21 +10,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		THE HOOOMEPAGE
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kqed' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
+	<!-- Homepage-Content -->
+	<div class="homepage-content">
+		<div class="container containerv2">
+			<div class="entry-content">
+				<?php the_content(); ?>
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kqed' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+		</div>
+	</div>
 	<footer class="entry-footer">
 		<?php edit_post_link( esc_html__( 'Edit', 'kqed' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
