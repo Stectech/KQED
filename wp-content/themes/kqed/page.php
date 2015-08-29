115fbdbc6c9f7992ddb7705550a6b8717bdfbbad
Jary <jaryroxas@gmail.com>
2015-08-29 22:25:23 +0800

    about us configs

--- wp-content/themes/kqed/page.php
+++ wp-content/themes/kqed/page.php
@@ -18,16 +18,11 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'home' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
+				<?php if( is_front_page() ) : ?>
+					<?php get_template_part( 'template-parts/content', 'home' ); ?>
+				<?php else : ?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endif; ?>
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
