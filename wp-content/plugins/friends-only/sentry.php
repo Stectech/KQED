1fddfa2e1e339ab0f4a257daed8d550e1b978531
Jary <jaryroxas@gmail.com>
2015-09-03 13:23:35 +0800

    added plugin friends-only

---
+++ wp-content/plugins/friends-only/sentry.php
@@ -0,0 +1,331 @@
+<?php 
+
+// This function determines whether the user should be displayed the login page based on whether or not they have already authenticated themselves with Wordpress (i.e. Administrator logged in), or with the sentry.
+
+function fo_runSentry() {
+
+	// Define session key and start the session
+	define ('SESSION_KEY', md5(site_url()));
+	session_start();
+	session_name('fo_session');
+
+	// Check for https
+	
+	if (is_ssl()) {
+		$http_method = 'https://';
+	}
+	else {
+		$http_method = 'http://';
+	}
+
+	// Create site path URLs to test later for URL hacking or provide access to special pages (e.g. login or FeedWrangler)
+	
+	$base_WP_URI = strtolower(site_url()); // e.g. "http://blog.com/gabesblog"
+	$base_WP_path = parse_url($base_WP_URI, PHP_URL_PATH); // e.g. "/gabesblog"
+	$request_URI = strtolower($http_method.$_SERVER['SERVER_NAME'].htmlspecialchars($_SERVER['REQUEST_URI'])); // e.g. http://blog.com/gabesblog/my-trip-to-the-supermarket
+	$request_path = parse_url($request_URI, PHP_URL_PATH); // e.g. "/gabesblog/my-trip-to-the-supermarket"
+
+	// If the sesion variable has already been set, then don't show the sentry
+	if ($_SESSION['friends_only'] == SESSION_KEY) {
+		return;
+		}
+	else {
+		$_SESSION['friends_only'] = 'Not authenticated';
+	}
+
+	// If the user is logged in then don't show the sentry	
+	if (is_user_logged_in()) {
+		return;
+	}
+	// If the user is requesting media (mostly RSS readers and subscription emails), then let them view the media
+	elseif (strpos($request_path, $base_WP_path.'/wp-content/uploads') === 0) {
+		return;
+	}
+	// If this is a wp-cron request, then don't show the sentry (used for running scheduled tasks)
+	elseif (strpos($request_path, $base_WP_path.'/wp-cron.php') === 0) {
+		return;
+	}
+	// If the user is requesting a FeedWrangler feed, then don't show the sentry
+	elseif (strpos($request_path, $base_WP_path.'/?feed=') === 0) {
+		return;
+	}
+	// If the user is not logged in, but they are trying to log in, then let them see the login page
+	elseif ((strpos($request_path, $base_WP_path.'/wp-admin/') === 0) || (strpos($request_path, $base_WP_path.'/wp-login.php') === 0)) {
+		return;
+	}
+	// If the user is trying to access XML-RPC then don't show the sentry
+	elseif (strpos($request_path, $base_WP_path.'/xmlrpc.php') === 0) {
+		return;
+	}
+	// If the user is trying to run BackWPUp then don't show the sentry
+	elseif (($_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR']) && (strpos($request_URI, 'backwpup') > 0)) {
+		return;
+	}
+	
+	// Check that the user requested URL matches the Wordpress domain settings, and if not, change
+	if (($_SERVER['HTTP_HOST'] != parse_url($base_WP_URI,PHP_URL_HOST)))
+		{
+		$new_URI = str_replace($_SERVER['HTTP_HOST'], parse_url($base_WP_URI,PHP_URL_HOST), selfURL()); // Fix hostname
+		header('Location: '.$new_URI);
+		}
+
+	// Load the array of email addresses and clean it up (including removing invalid email addresses)
+	
+	$PERMITTED_ADDRESSES = explode(chr(13), get_option('email_list'));
+				
+	array_walk($PERMITTED_ADDRESSES, 'fo_cleanAddress');
+
+	// Load and clean the address to be notified by email, then sort them so any null addresses are at the end of the list
+
+	$notify_address = explode (',', get_option('notify_address'));
+	array_walk($notify_address, 'fo_cleanAddress');
+	array_walk($notify_address, 'fo_removeInvalidAddress');
+	array_filter($notify_address, 'strlen');
+	rsort($notify_address);
+		
+	// Process the user provided password
+	
+	if (isset($_POST['access_email'])) {
+	
+		$supplied_address = strtolower(trim($_POST['access_email']));
+	 
+		if ( strlen($supplied_address) == 0 ) { $supplied_address = 'null'; }
+	
+		if (!in_array($supplied_address, $PERMITTED_ADDRESSES)) {
+	  
+	  		//Send email notifying of FAILED login
+	
+			if (get_option('notify_fail') && is_email($notify_address[0]) && $supplied_address != 'blank') {
+			wp_mail( $notify_address, "[".wp_specialchars_decode(get_bloginfo('name'), ENT_QUOTES)."] FAIL for ".$supplied_address,
+	  		"Failed login at ".wp_specialchars_decode(get_bloginfo('name'), ENT_QUOTES)." by ".$supplied_address."\n\nTime: ".date("H:i:s \(\G\M\TP\)")."\nIP: ".$_SERVER['REMOTE_ADDR']." (http://whatismyipaddress.com/ip/".$_SERVER['REMOTE_ADDR'].")");
+			}
+		
+			fo_showLoginForm(get_option('prompt_error'));
+	  
+	  	}
+		else {
+			// set session variable if password was validated
+			$_SESSION['friends_only'] = SESSION_KEY;
+
+			// Clear password protector variables
+			unset($_POST['access_login']);
+			unset($_POST['access_password']);
+			unset($_POST['Submit']);
+
+			// Send email notifying of SUCCESSFUL login
+		
+			if (get_option('notify_success') && is_email($notify_address[0])) {
+		    wp_mail( $notify_address, "[".wp_specialchars_decode(get_bloginfo('name'), ENT_QUOTES)."] SUCCESS for ".$supplied_address, "Successful login at ".wp_specialchars_decode(get_bloginfo('name'), ENT_QUOTES)." by ".$supplied_address."\n\nTime: ".date("H:i:s \(\G\M\TP\)")."\nIP: ".$_SERVER['REMOTE_ADDR']." (http://whatismyipaddress.com/ip/".$_SERVER['REMOTE_ADDR'].")", "From: Friends Only Notification <".get_settings('admin_email').">");
+	    	}    
+	  	}
+	}
+	
+	// If the user hasn't authenticated yet, show the login form	
+	else {
+    	fo_showLoginForm('');
+	}
+}
+
+// This function displays the login form if the user is required to authenticate with the sentry
+
+function fo_showLoginForm($error_message) {
+	
+	echo "<!DOCTYPE html>
+	<html ";
+	
+	echo language_attributes();
+	
+	echo ">
+	<head>
+	<title>";
+	
+	echo htmlentities(bloginfo('name'));
+	
+	echo "
+	</title>
+	  <META HTTP-EQUIV='CACHE-CONTROL' CONTENT='NO-CACHE'>
+	  <META HTTP-EQUIV='PRAGMA' CONTENT='NO-CACHE'>
+	  <META HTTP-EQUIV='CONTENT-TYPE' CONTENT='";
+
+	echo bloginfo('html_type');
+	echo " CHARSET=";
+	echo bloginfo('charset');
+	echo "'>";
+	
+	wp_head();
+	
+	// Check whether the user has specified their own CSS file. If not, use the default CSS styling.
+	
+	if (strlen(get_option('css_link')))	{
+		echo "<link rel='stylesheet' type='text/css' href='";
+		echo get_option('css_link');
+		echo "'>";
+	}
+	else	{
+		echo "	<style type='text/css'>
+		
+		// Reset the CSS styles using the Meyer Reset (http://meyerweb.com/eric/tools/css/reset/) before applying CSS styles
+		
+		/* This is the default Friends Only CSS. This CSS will be removed if you specify a custom CSS file in the Settings. */
+				
+		html, body, div, span, applet, object, iframe,
+		h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+		a, abbr, acronym, address, big, cite, code,
+		del, dfn, em, img, ins, kbd, q, s, samp,
+		small, strike, strong, sub, sup, tt, var,
+		b, u, i, center,
+		dl, dt, dd, ol, ul, li,
+		fieldset, form, label, legend,
+		table, caption, tbody, tfoot, thead, tr, th, td,
+		article, aside, canvas, details, embed, 
+		figure, figcaption, footer, header, hgroup, 
+		menu, nav, output, ruby, section, summary,
+		time, mark, audio, video {
+			margin: 0;
+			padding: 0;
+			border: 0;
+			font-size: 100%;
+			font: inherit;
+			vertical-align: baseline;
+		}
+		article, aside, details, figcaption, figure, 
+		footer, header, hgroup, menu, nav, section {
+			display: block;
+		}
+		body {
+			line-height: 1;
+		}
+		ol, ul {
+			list-style: none;
+		}
+		blockquote, q {
+			quotes: none;
+		}
+		blockquote:before, blockquote:after,
+		q:before, q:after {
+			content: '';
+			content: none;
+		}
+		table {
+			border-collapse: collapse;
+			border-spacing: 0;
+		}
+		
+		body {
+			font-family : Tahoma, Verdana, Arial;
+			font-color: #000000;
+			padding-left: 20%;
+			padding-top:50px;
+			padding-bottom: 50px;
+			padding-right: 5%;
+			background-color: #FFFFFF;
+		}
+		
+		h1	{
+			font-size: 2em; 
+			padding-bottom: 50px;
+			font-weight: normal;
+		}
+		
+		p	{
+			padding-top: 8px;
+			padding-bottom: 8px;
+		}
+		
+		p.prompt	{
+		}
+		
+		p.error	{
+			color: #AA0000;
+		}
+		
+		#form {
+			padding-top: 8px;
+			padding-bottom: 8px;
+		}
+		
+		input.input_email	{
+			font-size: 1.2em;
+		}
+		
+		input.input_submit	{
+			background-color: #DDDDDD; 
+			border-color: #AAAAAA; 
+			color: #000000; 
+			font-family: tahoma, verdana, arial; 
+			font-size: 1.2em;
+		}
+		
+		.admin	{
+			padding-top: 20px;
+		}
+		
+		.adminlink {
+			color: #CCCCCC; font-size: small;
+		}
+	
+		</style>\n";
+	}
+		
+	echo "</head>
+	
+	<body>
+	
+	<h1>";
+	
+	echo bloginfo('name');
+				
+	echo "</h1>\n";
+	
+	if ($error_message == NULL) {
+		echo get_option('prompt_email');
+	}
+	else {
+		echo $error_message; 
+	}
+		
+	echo "
+	<div id='form'>
+	<form method='post'>\n<input class='input_email' type='text' name='access_email' size='32' />&nbsp;<input type='submit' class='input_submit' name='Submit' value='";
+	
+	echo get_option('prompt_submit');
+	
+	echo "' />\n</form>
+	</div>
+	<p class='admin'><a class='adminlink' href='";
+	
+	echo site_url();
+	
+	echo "/wp-admin/'>";
+	
+	echo get_option('prompt_admin');
+	
+	echo "</a></p>
+	</body>
+	</html>";
+	
+	// stop at this point
+	die();
+}
+
+function fo_cleanAddress(&$value, $key) 
+	{ 
+	    $value = strtolower(trim($value));
+	}
+	
+function fo_removeInvalidAddress (&$value, $key)
+	{
+    if (!is_email($value)) $value = NULL;
+	}
+	
+function selfURL() 
+{ 
+    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : ""; 
+    $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s; 
+    $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]); 
+    return $protocol."://".$_SERVER['SERVER_NAME'].$port.htmlspecialchars($_SERVER['REQUEST_URI']); 
+} 
+
+function strleft($s1, $s2) { return substr($s1, 0, strpos($s1, $s2)); }
+
+?>
---
+++ wp-content/plugins/friends-only/settings.php
@@ -0,0 +1,108 @@
+<?php
+
+// Create menu
+
+add_action('admin_menu', 'friendsonly_createmenu');
+
+function friendsonly_createmenu() {
+
+	add_options_page('Friends Only Options', 'Friends Only', 'manage_options', 'friendsonly-settings', 'friendsonly_options');
+
+	//call register settings function
+	add_action( 'admin_init', 'register_friendsonlysettings' );
+}
+
+
+// Create settings
+
+function register_friendsonlysettings() {
+	//register our settings
+	register_setting( 'friendsonly-main', 'email_list' );
+	register_setting( 'friendsonly-main', 'prompt_email' );
+	register_setting( 'friendsonly-main', 'prompt_error' );
+	register_setting( 'friendsonly-main', 'prompt_submit' );
+	register_setting( 'friendsonly-main', 'notify_success' );
+	register_setting( 'friendsonly-main', 'notify_fail' );
+	register_setting( 'friendsonly-main', 'notify_address' );
+	register_setting( 'friendsonly-main', 'prompt_admin' );
+	register_setting( 'friendsonly-main', 'css_link' );
+	
+	//Set defaults if any settings are empty
+	if (trim(get_option('prompt_email')) == '') update_option('prompt_email', '<p class="prompt">Please enter your email address</p>');
+	if (trim(get_option('prompt_error')) == '') update_option('prompt_error', '<p class="error">Unrecognized email address. Please try again.</p>');
+	if (trim(get_option('prompt_submit')) == '') update_option('prompt_submit', 'Login');
+	if (trim(get_option('prompt_admin')) == '') update_option('prompt_admin', 'Administrator login &gt;&gt;');
+	if (trim(get_option('css_link')) == '') update_option('css_link', '');
+	add_option ('notify_success', 1);
+	add_option ('notify_fail', 1);
+	if (trim(get_option('notify_address')) == '') update_option('notify_address', get_bloginfo('admin_email'));
+}
+	
+// Show settings page
+
+function friendsonly_options() {
+
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+?>
+
+<div class="wrap">
+<h2>Friends Only Options</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'friendsonly-main' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Address list</th>
+        <td><textarea rows="18" cols="40" name="email_list"><?php echo get_option('email_list'); ?></textarea>
+		</td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Initial prompt</th>
+        <td><textarea rows="2" cols="40" name="prompt_email"><?php echo htmlentities(get_option('prompt_email'), ENT_QUOTES); ?></textarea>
+		</td>
+
+        <tr valign="top">
+        <th scope="row">Error message</th>
+        <td><textarea rows="2" cols="40" name="prompt_error"><?php echo htmlentities(get_option('prompt_error'), ENT_QUOTES); ?></textarea>
+		</td>
+
+        <tr valign="top">
+        <th scope="row">Submit button</th>
+        <td><input type="text" size="40" name="prompt_submit" value="<?php echo htmlentities(get_option('prompt_submit'), ENT_QUOTES); ?>" />
+		</td>
+		</tr>
+
+        <tr valign="top">
+        <th scope="row">Administrator link</th>
+        <td><input type="text" size="40" name="prompt_admin" value="<?php echo htmlentities(get_option('prompt_admin'), ENT_QUOTES); ?>" />
+		</td>
+		</tr>
+
+        <tr valign="top">
+        <th scope="row">Email notification</th>
+        <td><input type="checkbox" name="notify_success" value="1" <?php if (get_option('notify_success')) echo 'checked'; ?>/> Notify on successful login<br />
+        <input type="checkbox" name="notify_fail" value="1" <?php if (get_option('notify_fail')) echo 'checked'; ?>/> Notify on failed login<br />
+        Email address: <input type="text" name="notify_address" size="40" value="<?php echo get_option('notify_address') ?>" />
+		</td>
+		</tr>
+ 
+        <tr valign="top">
+        <th scope="row">External CSS (URL)</th>
+        <td><input type="text" name="css_link" size="40" value="<?php echo htmlentities(get_option('css_link'), ENT_QUOTES); ?>" />
+		</td>
+		</tr>
+        
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+</form>
+</div>
+
+<?php 
+}
