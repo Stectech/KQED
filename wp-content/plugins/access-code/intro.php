365ef514377e7e85f7ca71d6c30c416cd04a19c2
Jary <jaryroxas@gmail.com>
2016-01-14 21:38:44 +0800

    fix welcome

--- wp-content/plugins/access-code/intro.php
+++ wp-content/plugins/access-code/intro.php
@@ -88,7 +88,7 @@
 				</li>
 				<li>
 					<div class="inner-wrapper">
-						<h3>
+						<h3> 
 							<span class="welcome">Welcome to the future. </span> <br>
 							KQED is transforming today to meet the needs of tomorrow’s Bay Area.
 						</h3>
